16be413df0d1b459524b023596d685c1134a6b62
emer mcmahon <emerod0000@gmail.com>
2018-05-04 13:28:26 +0100

    updating code comments

--- js/app.js
+++ js/app.js
@@ -1,42 +1,37 @@
-/*
- * Create a list that holds all of your cards
- */
-
-/*definingcards*/
+ //Create a list that holds all of your cards//
+//definingcards by their class//
 let card = document.getElementsByClassName("card");
-/*
- * Add empty array of cards
- */
+
+
+//Add empty array of cards//
 let cards = [...card];
 
-console.log(cards); /*testing it prints an array*/
+console.log(cards); //testing it prints an array//
 
 
-/*
- * Add empty array of opened cards
- */
+
+//Add empty array of opened cards//
 var openedCards = [];
 
 
-/*defining deck*/
+//defining deck from element//
 const deck = document.getElementById("deckOfCards");
 
 
-/*moves*/
+//Defining moves using query selector//
 let moves= 0;
 let movesCounter =document.querySelector('.moves');
 
-/*stars*/
+//starsusing query selector//
 let stars = "";
 let starsCounter =document.querySelector('.stars');
 
 
 
+ // Display the cards on the page//
+ //shuffle the list of cards using the provided "shuffle" method below//
+// Shuffle function from http://stackoverflow.com/a/2450976//
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
-// Shuffle function from http://stackoverflow.com/a/2450976*/
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -54,16 +49,14 @@ function shuffle(array) {
 }
 
 
-/*
- * Begin the game function
- */
 
-
-
+ //Begin the game function//
  function playGame(){
 
 var shuffleDeck = shuffle(cards);
 
+ //loop through each card and create its HTML//
+ //add each card's HTML to the page//
 var i;
 for (var i = 0; i < shuffleDeck.length; i++) {
 	deck.innerHTML = "";
@@ -78,21 +71,11 @@ cards[i].classList.remove("show", "open", "match", "disable");
  console.log("playGame function working");
  }
 
- window.onload = playGame();
-
-/*
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
- 
+ window.onload = playGame();//on load of browser game should commence//
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- will go to funtion showCard
- */
-
- 
+//set up the event listener for a card. If a card is clicked://
+//will go to funtion showCard//
  for (var i = 0; i < cards.length; i++) {
 
  cards[i].addEventListener("click", showCard);
@@ -103,11 +86,7 @@ cards[i].classList.remove("show", "open", "match", "disable");
 };
 
 
- /*
- *  - display the card's symbol (put this functionality in another function that you call from this one)
-  */
-
-
+ //display the card's symbol (put this functionality in another function that you call from this one)//
 function showCard(){
 this.classList.toggle("open");
 this.classList.toggle("show");
@@ -115,12 +94,9 @@ this.classList.toggle("disable");
 
  console.log("showCard function working");
 
-
 };
 
- /*
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- */
+//add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)//
 function openCards() {
     openedCards.push(this);
     var num = openedCards.length;
@@ -140,13 +116,13 @@ console.log("opecards function working");
 };
  
 
-
+ //increment the move counter and display it on the page (put this functionality in another function that you call from this one)
 function countMoves(){
 
-  moves ++; //moves the counter up one after each card is clicked
-   movesCounter.innerHTML = moves; //displays the values of moves on the page
+  moves ++; //moves the counter up one after each card is clicked//
+   movesCounter.innerHTML = moves; //displays the values of moves on the page//
 
-//If staement for star colours to change based on the number of moves it takes the user to complete the game
+//If staement for star colours to change based on the number of moves it takes the user to complete the game//
    if(moves == 16){
    document.querySelector('.stars').style.color = "green";
 }else if 
@@ -159,25 +135,17 @@ function countMoves(){
     document.querySelector('.stars').style.color = "black";
 }
 
-
-
-
-  /*
  
- *  - if the list already has another card, check to see if the two cards match
-*/
-
+ // if the list already has another card, check to see if the two cards match//
 function cardsMatched(){
     openedCards[0].classList.add("match", "disable");
     openedCards[1].classList.add("match", "disable");
-  
   
  console.log("Match function working");
 
 };
 
-
-  /*
+ // if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)//
 function noMatch(){
     
     openedCards[0].classList.add("unmatched");
@@ -192,7 +160,7 @@ function noMatch(){
      console.log("noMatch function working");
 };
 
-*/
+//if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)//
 
 function disable(){
     Array.prototype.filter.call(cards, function(card){
@@ -200,31 +168,13 @@ function disable(){
     });
 }
 
+//   //
 function enable(){
 
 
 
 }
 
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
- /*
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+ //if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)//
+ 
  
